50b0d7402414ad855bffd47ff60ec832d0822cf1
Ben Manley <benjamintmanley@gmail.com>
2022-02-23 20:23:41 -0600

    reconfigure turtle router

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ import { Router } from 'express'
 const router = Router()
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/banana', function(req, res) {
   res.render('index', { title: 'Express' })
 })
+
+
 
 export { 
   router
--- routes/turtles.js
+++ routes/turtles.js
@@ -3,7 +3,7 @@ const router = Router()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource')
+  res.send(`turtles are cool`)
 })
 
 export {
